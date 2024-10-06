4e615ec3e0769b1b8fb230b43d7ab3b993bcc42b
handoverhandprompting <150869385+handoverhandprompting@users.noreply.github.com>
2024-10-06 16:25:17 +0800

    Update util.py

--- models/util.py
+++ models/util.py
@@ -229,18 +229,21 @@ def predict_plot(hr1: float, hr2: float):
     plt.xlabel('Years after Sacubitril/Valsartan Initiation')
     plt.ylabel('Survival Probability')
 
+    # 確保使用唯一的時間點
+    baseline_hazard = baseline_hazard.drop_duplicates(subset=['time']).sort_values(by='time')
+    
     # 使用基準風險中的時間列來設定 X 軸的刻度
     plt.plot(baseline_hazard['time'], predicted_survival1, color='blue')
     plt.plot(baseline_hazard['time'], predicted_survival2, color='red')
     
-    # 設定 X 軸的刻度，使用唯一的時間點
-    plt.xticks(baseline_hazard['time'][::12])  # 每隔 12 個月設置一個刻度
+    # 設定 X 軸的刻度，根據時間列每隔 12 個月設置一個刻度
+    plt.xticks(baseline_hazard['time'][::12])  # 每 12 個月設置一個刻度
     
-    # 設定 X 軸的上限，根據實際的時間範圍
+    # 設定 X 軸的上限，根據實際的時間範圍，而不是索引
     plt.xlim(baseline_hazard['time'].min(), baseline_hazard['time'].max())
     
-    # 設定 X 軸的刻度標籤為年份，假設你的時間列是以月份為單位的
-    plt.gca().xaxis.set_major_formatter(ticker.FuncFormatter(lambda x, _: int(x/12)))  # 將刻度除以 12 以得到年份
+    # 設定 X 軸的刻度標籤為月份或年份
+    plt.gca().xaxis.set_major_formatter(ticker.FuncFormatter(lambda x, _: '{:g}'.format(x/12)))  # 格式化顯示為月份或年份
 
     # 設定 Y 軸的刻度，從 0 到 1，每 0.1 一個刻度
     y_intervals = np.arange(0, 1.1, 0.1)
