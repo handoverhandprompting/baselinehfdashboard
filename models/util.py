541da6f23e03ba71a605318423524367fcb7b360
handoverhandprompting <150869385+handoverhandprompting@users.noreply.github.com>
2024-10-12 02:24:47 +0900

    Update util.py

--- models/util.py
+++ models/util.py
@@ -224,11 +224,11 @@ def predict_plot(hr1: float, hr2: float):
     predicted_survival2 = baseline_survival ** hr2
 
     # 使用基準風險中的時間列來繪製曲線
-    try:
+    # try:
         plt.plot(baseline_hazard['time'], predicted_survival1, color='blue', label='Scenario 1')
         plt.plot(baseline_hazard['time'], predicted_survival2, color='red', label='Scenario 2')
-    except:
-        pass
+    # except:
+        # pass
     plt.title('')
     plt.plot(baseline_hazard['time'], baseline_survival, color='black')
     plt.xlabel('Years after Sacubitril/Valsartan Initiation')
