f38ada54a07703a388a0edff6472c93d8307a0af
handoverhandprompting <150869385+handoverhandprompting@users.noreply.github.com>
2024-10-17 20:39:45 +0800

    Update prediction_model.py

--- models/prediction_model.py
+++ models/prediction_model.py
@@ -1,25 +1,53 @@
 import streamlit as st
-from models.util import calculate_and_set, predict_plot
+from models.util import calculate_and_set, predict_plot, HrControl
 
 
 # ==========
 def prediction_view():
     st.subheader('Survival Prediction')
-    if 'risk_value_col1_pred' not in st.session_state:
-        st.session_state['risk_value_col1_pred'] = 1.0
-        hr1 = st.session_state['risk_value_col1_pred']
+    # Set "pred_copy" related controls
+    if "pred_copy" not in st.session_state:
+        # If baseline and prediction all not input
+        st.session_state['pred_copy'] = False
 
-    else:
-        hr1 = st.session_state['risk_value_col1_pred']
+    # If copy from explanation is activated
+    if st.session_state['pred_copy']:
+        # blue copy if there's value in baseline model
+        if 'risk_value_col1' in st.session_state:
+            hr1 = st.session_state['risk_value_col1']
+        # blue set to 1 if copy from baseline clicked but baseline not set
+        else:
+            hr1 = 1
 
-    if 'risk_value_col2_pred' not in st.session_state:
-        st.session_state['risk_value_col2_pred'] = 1.0
-        hr2 = st.session_state['risk_value_col2_pred']
+        if 'risk_value_col2' in st.session_state:
+            hr2 = st.session_state['risk_value_col2']
+        else:
+            hr2 = 1
+
+    # If copy from explanation is not activated
     else:
-        hr2 = st.session_state['risk_value_col2_pred']
+        if 'risk_value_col1_pred' not in st.session_state:
+            st.session_state['risk_value_col1_pred'] = HrControl.risk_calculation()
+            hr1 = st.session_state['risk_value_col1_pred']
+
+        else:
+            hr1 = st.session_state['risk_value_col1_pred']
+
+        if 'risk_value_col2_pred' not in st.session_state:
+            st.session_state['risk_value_col2_pred'] = 1.0
+            hr2 = st.session_state['risk_value_col2_pred']
+        else:
+            hr2 = st.session_state['risk_value_col2_pred']
+
+    # Determine if showblue or showred button is clicked
+    if 'showblue' not in st.session_state:
+        st.session_state['showblue'] = False
+    if 'showred' not in st.session_state:
+        st.session_state['showred'] = False
 
     # plotting
-    prediction = predict_plot(hr1, hr2)
+    prediction = predict_plot(hr1, hr2, st.session_state['showblue'], st.session_state['showred'])
+    print('blue/red: ', st.session_state['showblue'],  st.session_state['showred'])
     st.pyplot(prediction)
 
     # place the holder here
@@ -27,7 +55,11 @@ def prediction_view():
 
     # Hight, weight and BMI
     with st.expander(':man-frowning: :violet[Baseline status]'):
-        age = st.number_input('Age', step=1, key='age_pred')
+        age = st.number_input('Age', step=1, key='age_pred', value=None)
+
+        if age is None:
+            age = 65
+
         bmi = 0
         col1, col1_1, col2, col2_2 = st.columns([5, 2, 5, 2])
         with col1_1:
@@ -35,17 +67,20 @@ def prediction_view():
         with col2_2:
             weight_unit = st.selectbox("Weight_unit", ["kg", "lbs"], key='weight-unit_pred')
         with col1:
-            height = st.number_input('Height ', step=1, value=160, key='height_pred')
+            height = st.number_input('Height ', step=1, value=None, key='height_pred')
             if height:
                 if height_unit == "in":
                     height = height * 2.54
         with col2:
-            weight = st.number_input('Weight ', step=1, value=60, key='weight_pred')
+            weight = st.number_input('Weight ', step=0.01, value=None, key='weight_pred')
             if weight:
                 if weight_unit == "lbs":
                     weight = weight * 0.45359237
         if height and weight:
             bmi = round(weight / ((height / 100) ** 2), 2)
+            st.session_state['bmi'] = bmi
+        else:
+            st.session_state['bmi'] = 28
 
         col1, col2, col3 = st.columns([1, 3, 4])
         with col1:
@@ -63,7 +98,7 @@ def prediction_view():
                                                  ['Class II (Slight limitation of physical activity. Comfortable at rest. Ordinary physical activity results in fatigue, palpitation, shortness of breath or chest pain.)', 2],
                                                  ['Class III (Marked limitation of physical activity. Comfortable at rest. Less than ordinary activity causes fatigue, palpitation, shortness of breath or chest pain.)', 3],
                                                  ['Class IV (Symptoms of heart failure at rest. Any physical activity causes further discomfort.)', 4]], format_func=lambda x: x[0],
-                                        help='New York Heart Association functional classification', index=0, key='nyha_display_pred')
+                                        help='New York Heart Association functional classification', index=2, key='nyha_display_pred')
             nyha = nyha_display[1]
             paod_diplay = st.selectbox('PAOD', [['yes', 1], ['no', 0]], format_func=lambda x: x[0], index=1, help='Peripheral Arterial Occlusive Disease', key='paod_diplay_pred')
             paod = paod_diplay[1]
@@ -76,8 +111,8 @@ def prediction_view():
         col1, col2 = st.columns([5, 2])
         with col1:
             acei_display = st.selectbox('ACEI/ARB',
-                                        ['None', 'valsartan', 'losartan', 'captopril', 'enalapril', 'ramipril', 'Not mentioned above'],
-                                        help='Angiotensin Converting Enzyme Inhibitors/Angiotensin Receptor Blockers', index=0, key='acei_display_pred')
+                                        ['None', 'Valsartan', 'Losartan', 'Captopril', 'Enalapril', 'Ramipril', 'Not mentioned above'],
+                                        help='Angiotensin Converting Enzyme Inhibitors/Angiotensin Receptor Blockers', index=1, key='acei_display_pred')
         with col2:
             acei_dose = st.number_input('Dose(mg)', disabled=(acei_display == 'None' or acei_display == 'Not mentioned above'), value=None, key='acei_dose_pred')
             if acei_dose:
@@ -92,12 +127,12 @@ def prediction_view():
                 else:
                     total_acei = 0
             else:
-                total_acei = 141
+                total_acei = 80
 
         col1, col2 = st.columns(2)
         with col1:
             en_h_display = st.selectbox('Initiation time of Entresto(sacubitril/valsartan)',
-                                        ['Outpatient Department (OPD)', 'Inpatient Department (IPD)', 'None'], key='en_h_display_pred')
+                                        ['Outpatient Department (OPD)', 'Inpatient Department (IPD)', 'None'], key='en_h_display_pred', index=0)
         with col2:
             ua_u_o_display = st.selectbox('Urate-lowering Agents', ['None', 'allopurinol', 'benzbromarone', 'febuxostat', 'probenecid', 'rasburicase', 'sulfinpyrazone', 'Not mentioned above'], index=0, key='ua_u_o_display_pred')
             if ua_u_o_display == 'None' or ua_u_o_display == 'Not mentioned above':
@@ -119,13 +154,19 @@ def prediction_view():
                 st.write(f'###### BUN ######')
                 bun_none = st.checkbox('None', key='bun_none_pred', value=True)
             with col1_1:
-                bun = st.number_input('(mg/dL)', disabled=bun_none, help='Blood Urea Nitrogen', value=20, key='bun_pred')
+                bun = st.number_input('(mg/dL)', disabled=bun_none, help='Blood Urea Nitrogen', value=None, key='bun_pred', format="%0.2f")
+
+            if bun is None:
+                bun = 30
 
             with col2:
                 st.write(f'###### NT_proBNP ######')
                 nt_proBNP_none = st.checkbox('None', key='nt_proBNP_none_pred', value=True)
             with col3:
-                nt_proBNP = st.number_input('(pg/mL)', disabled=nt_proBNP_none, help='N-Terminal Pro-Brain (or B-type) Natriuretic Peptide', value=750, key='nt_proBNP_pred')
+                nt_proBNP = st.number_input('(pg/mL)', disabled=nt_proBNP_none, help='N-Terminal Pro-Brain (or B-type) Natriuretic Peptide', value=None, key='nt_proBNP_pred', format="%0.2f")
+
+            if nt_proBNP is None:
+                nt_proBNP = 1500
 
         st.write(' ')
 
@@ -135,13 +176,19 @@ def prediction_view():
                 st.write(f'###### ALT ######')
                 alt_none = st.checkbox('None', key='alt_none_pred', value=True)
             with col1_1:
-                alt = st.number_input('(U/L)', disabled=alt_none, key='alt_pred', help='Alanine Aminotransferase', value=20.3)
+                alt = st.number_input('(U/L)', disabled=alt_none, key='alt_pred', help='Alanine Aminotransferase', value=None, format="%0.2f")
+
+            if alt is None:
+                alt = 30
 
             with col2:
                 st.write(f'###### RDW_CV ######')
                 rdw_cv_none = st.checkbox('None', key='rdw_cv_none_pred', value=True)
             with col2_1:
-                rdw_cv = st.number_input('(%)', disabled=rdw_cv_none, key='rdw_cv_pred', help='Red Cell Distribution Width_Coefficient of Variation', value=15.5)
+                rdw_cv = st.number_input('(%)', disabled=rdw_cv_none, key='rdw_cv_pred', help='Red Cell Distribution Width_Coefficient of Variation', value=None, format="%0.2f")
+
+            if rdw_cv is None:
+                rdw_cv = 14.5
 
     with st.expander('❤️ :red[Cardiac parameters of echocardiography]'):
         with st.container():
@@ -151,13 +198,16 @@ def prediction_view():
                 ar_none = st.checkbox('None', key='ar_none_pred', value=True)
             with col1_1:
                 ar = st.selectbox(' ', [['trace/trivial', 0.5], ['mild', 1], ['mild to moderate', 1.5], ['moderate', 2],
-                                        ['moderate to severe', 2.5], ['severe', 3]], label_visibility='visible', disabled=ar_none, help='Aortic Regurgitation', format_func=lambda x: x[0], key='ar_pred')
+                                        ['moderate to severe', 2.5], ['severe', 3]], label_visibility='visible', disabled=ar_none, help='Aortic Regurgitation', format_func=lambda x: x[0], key='ar_pred', index=2)
                 ar_value = ar[1]
             with col2:
                 st.write(f'###### RV ######')
                 rvdd_none = st.checkbox('None', key='rvdd_none_pred', value=True)
             with col2_1:
-                rvdd = st.number_input('(cm)', key='rvdd_pred', disabled=rvdd_none, help='RVDd, Right Ventricular Diastolic Dimension', value=3.5)
+                rvdd = st.number_input('(cm)', key='rvdd_pred', disabled=rvdd_none, help='RVDd, Right Ventricular Diastolic Dimension', value=None, format="%0.2f")
+
+            if rvdd is None:
+                rvdd = 3.2
 
         with st.container():
             col1, col1_1, space1, col2, col2_1 = st.columns([3, 6, 3, 3, 6])
@@ -165,13 +215,19 @@ def prediction_view():
                 st.write(f'###### IVSd ######')
                 ivsd_none = st.checkbox('None', key='ivsd_none_pred', value=True)
             with col1_1:
-                ivsd = st.number_input('(cm)', key='ivsd_pred', disabled=ivsd_none, help='Interventricular Septum Dimension', value=1.5)
+                ivsd = st.number_input('(cm)', key='ivsd_pred', disabled=ivsd_none, help='Interventricular Septum Dimension', value=None, format="%0.2f")
+
+            if ivsd is None:
+                ivsd = 1.2
 
             with col2:
                 st.write(f'###### LVMI ######')
                 lvmi_none = st.checkbox('None', key='lvmi_none_pred', value=True)
             with col2_1:
-                lvmi = st.number_input('(g/m2)', key='lvmi_pred', disabled=lvmi_none, label_visibility='visible', help='Left Ventricular Mass Index', value=150.5)
+                lvmi = st.number_input('(g/m2)', key='lvmi_pred', disabled=lvmi_none, label_visibility='visible', help='Left Ventricular Mass Index', value=None, format="%0.2f")
+
+            if lvmi is None:
+                lvmi = 12.5
 
         with st.container():
             col1, col1_1, space1, col2, col2_1 = st.columns([3, 6, 3, 3, 6])
@@ -179,13 +235,19 @@ def prediction_view():
                 st.write(f'###### ESD ######')
                 esd_none = st.checkbox('None', key='esd_none_pred', value=True)
             with col1_1:
-                esd = st.number_input('(cm)', key='esd_pred', disabled=esd_none, help='End Systolic Dimension = LVIDs, Left Ventricular Internal Diameter End Systole', value=5.5)
+                esd = st.number_input('(cm)', key='esd_pred', disabled=esd_none, help='End Systolic Dimension = LVIDs, Left Ventricular Internal Diameter End Systole', value=None, format="%0.2f")
+
+            if esd is None:
+                esd = 5.5
 
             with col2:
                 st.write(f'###### LAD ######')
                 lad_none = st.checkbox('None', key='lad_none_pred', value=True)
             with col2_1:
-                lad = st.number_input('(cm)', key='lad_pred', disabled=lad_none, label_visibility='visible', help='LAD, Left Atrial Diameter', value=4.35)
+                lad = st.number_input('(cm)', key='lad_pred', disabled=lad_none, label_visibility='visible', help='LAD, Left Atrial Diameter', value=None, format="%0.2f")
+
+            if lad is None:
+                lad = 4.5
 
         with st.container():
             col1, col1_1, space1, col2, col2_1 = st.columns([3, 6, 3, 3, 6])
@@ -193,8 +255,11 @@ def prediction_view():
                 st.write(f'###### LVEF_2D ######')
                 lvef_2d_none = st.checkbox('None', key='lvef_2d_none_pred', value=True)
             with col1_1:
-                lvef_2d = st.number_input('(%)', key='lvef_2d_pred', disabled=lvef_2d_none, label_visibility='visible', help='Left Ventricular Ejection Fraction_2D = EF MOD-sp4, Ejection Fraction Method of Disks-Single Plane, Apical 4 Chamber', value=35.5)
-    # TODO:Change button layout
+                lvef_2d = st.number_input('(%)', key='lvef_2d_pred', disabled=lvef_2d_none, label_visibility='visible', help='Left Ventricular Ejection Fraction_2D = EF MOD-sp4, Ejection Fraction Method of Disks-Single Plane, Apical 4 Chamber', value=None, format="%0.2f")
+
+            if lvef_2d is None:
+                lvef_2d = 30
+
 
     # Assign what shall do in enter_col1 and enter_col2
     st.markdown("""
@@ -204,7 +269,7 @@ def prediction_view():
  }</style>""", unsafe_allow_html=True)
     with enter_col1:
         st.markdown('<span id="button-col1"></span>', unsafe_allow_html=True)
-        st.button('Enter', 'enter1_pred', on_click=calculate_and_set, args=(
+        left_button = st.button('Enter', 'enter1_pred', on_click=calculate_and_set, args=(
             dialysis, bun, age, lvef_2d_none, lvef_2d, esd_none, esd, rdw_cv_none, rdw_cv, ivsd_none, ivsd, bmi, lvmi_none, lvmi, nt_proBNP_none, nt_proBNP, paod, total_acei, p2y12, ar_none, ar_value, en_h_display, nyha, rvdd_none, rvdd, ua_u_0, alt_none, alt, lad_none, lad, 'risk_value_col1_pred'),
                   type='primary')
 
@@ -215,6 +280,22 @@ def prediction_view():
  }</style>""", unsafe_allow_html=True)
     with enter_col2:
         st.markdown('<span id="button-col2"></span>', unsafe_allow_html=True)
-        st.button('Enter', 'enter2_pred', on_click=calculate_and_set, args=(
+        right_button = st.button('Enter', 'enter2_pred', on_click=calculate_and_set, args=(
             dialysis, bun, age, lvef_2d_none, lvef_2d, esd_none, esd, rdw_cv_none, rdw_cv, ivsd_none, ivsd, bmi, lvmi_none, lvmi, nt_proBNP_none, nt_proBNP, paod, total_acei, p2y12, ar_none, ar_value, en_h_display, nyha, rvdd_none, rvdd, ua_u_0, alt_none, alt, lad_none, lad, 'risk_value_col2_pred'),
                   type='secondary')
+
+    if left_button:
+        st.session_state['showblue'] = True
+        st.experimental_rerun()
+    if right_button:
+        st.session_state['showred'] = True
+        st.experimental_rerun()
+
+    copy_explain_btn = st.button('Copy from Explanation')
+    if copy_explain_btn:
+        st.session_state['pred_copy'] = True
+        st.session_state['showblue'] = True
+        st.session_state['showred'] = True
+        st.experimental_rerun()
+    # TODO: debug
+    print(st.session_state['risk_value_col1_pred'])
